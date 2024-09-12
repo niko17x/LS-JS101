e8936773d2c01f521bcb3e139f48e8ec2118f9dc
Niko Shin <nshin17@gmail.com>
2024-09-11 18:32:28 -0700

    Initial commit

---
+++ js.js
@@ -0,0 +1,9 @@
+function invalidNumber(number) {
+  return Number.isNaN(Number(number));
+}
+
+const arr = ["hello", "this", "is", "niko"];
+
+console.log(1 / 0);
+
+console.log("this is a testing for git development branch.");
