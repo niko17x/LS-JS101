90e2dcf508548b499fb55766d6a4205885b13919
Niko Shin <nshin17@gmail.com>
2024-09-18 18:16:38 -0700

    feat: Add function to check score for winner

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,51 +2,38 @@
 
 const readline = require("readline-sync");
 
-function prompt(message) {
-  console.log(`=> ${message}`);
-}
+let runGame = true;
+const VALID_CHOICES = ["rock", "paper", "scissors", "spock", "lizard"];
+const WIN_CONDITIONS = {
+  rock: ["scissors", "lizard"],
+  paper: ["rock", "spock"],
+  scissors: ["paper", "lizard"],
+  lizard: ["spock", "paper"],
+  spock: ["scissors", "rock"],
+};
+let score = {
+  player: 0,
+  computer: 0,
+  tie: 0,
+};
 
-function playGame() {
-  const VALID_CHOICES = ["rock", "paper", "scissors"];
-
-  function displayWinner(choice, computerChoice) {
-    prompt(`You chose: ${choice}, computer chose: ${computerChoice}`);
-
-    if (
-      (choice === "rock" && computerChoice === "scissors") ||
-      (choice === "paper" && computerChoice === "rock") ||
-      (choice === "scissors" && computerChoice === "paper")
-    ) {
-      prompt("You win!");
-    } else if (
-      (choice === "rock" && computerChoice === "paper") ||
-      (choice === "paper" && computerChoice === "scissors") ||
-      (choice === "scissors" && computerChoice === "rock")
-    ) {
-      prompt("Computer wins!");
-    } else {
-      prompt("It's a tie");
+function checkWinner() {
+  for (let elem in score) {
+    if (score[elem] === 3) {
+      prompt(
+        `${elem} wins the game!\n#############################################`
+      );
+      score = {
+        player: 0,
+        computer: 0,
+        tie: 0,
+      };
+      replay();
     }
   }
-
-  prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
-  let choice = readline.question();
-
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
-    choice = readline.question();
-  }
-
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-
-  let computerChoice = VALID_CHOICES[randomIndex];
-
-  displayWinner(choice, computerChoice);
 }
 
-while (true) {
-  playGame();
-
+function replay() {
   prompt("Do you want to play again (y/n)?");
   let answer = readline.question().toLowerCase();
 
@@ -54,6 +41,57 @@ while (true) {
     prompt("Please enter 'y' or 'n'");
     answer = readline.question().toLowerCase();
   }
+  answer === "n" ? (runGame = false) : (runGame = true);
+}
 
-  if (answer === "n") break;
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function playGame() {
+  let foo = 0;
+
+  function displayWinner(playerChoice, computerChoice) {
+    prompt(`You chose: ${playerChoice}, computer chose: ${computerChoice}`);
+
+    if (playerChoice === computerChoice) {
+      prompt("It's a tie!");
+      return "tie";
+    } else if (WIN_CONDITIONS[playerChoice].includes(computerChoice)) {
+      prompt("You WIN!");
+      return "player";
+    } else {
+      prompt("You lose!");
+      return "computer";
+    }
+  }
+
+  function incrementScore(winner) {
+    score[winner] += 1;
+
+    if (score[winner] >= 3) {
+      prompt(`${winner} wins the game!`);
+    }
+  }
+
+  prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
+  let playerChoice = readline.question();
+
+  while (!VALID_CHOICES.includes(playerChoice)) {
+    prompt("That's not a valid choice");
+    playerChoice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  let winner = displayWinner(playerChoice, computerChoice);
+  incrementScore(winner);
+  console.log(score);
+}
+
+while (runGame) {
+  playGame();
+  checkWinner();
 }
