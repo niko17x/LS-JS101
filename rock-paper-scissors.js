afa920c389ebaf4a8d82bee434e1640122ba5f38
Niko Shin <nshin17@gmail.com>
2024-09-21 22:23:34 -0700

    feat: Add function to display game round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,251 +1,3 @@
-// // Lesson 2: Small Programs > 28. Rock Paper Scissors
-
-// const readline = require("readline-sync");
-
-// let runGame = true;
-// // const VALID_CHOICES = [
-// //   "(r)ock",
-// //   "(p)aper",
-// //   "(s)cissors",
-// //   "sp(o)ck",
-// //   "(l)izard",
-// // ];
-
-// // const WIN_CONDITIONS = {
-// //   r: ["s", "l"],
-// //   p: ["r", "o"],
-// //   s: ["p", "l"],
-// //   l: ["o", "p"],
-// //   o: ["s", "r"],
-// // };
-
-// // let score = {
-// //   player: 0,
-// //   computer: 0,
-// //   tie: 0,
-// // };
-
-// // function resetScore() {
-// //   score = {
-// //     player: 0,
-// //     computer: 0,
-// //     tie: 0,
-// //   };
-// // }
-
-// // function convertToWord(playerChoice) {
-// //   switch (playerChoice) {
-// //     case "s":
-// //       word = "scissors";
-// //       break;
-// //     case "r":
-// //       word = "rock";
-// //       break;
-// //     case "p":
-// //       word = "paper";
-// //       break;
-// //     case "o":
-// //       word = "spock";
-// //       break;
-// //     case "l":
-// //       word = "lizard";
-// //       break;
-// //     default:
-// //       null;
-// //   }
-// //   return word;
-// // }
-
-// // function displayRoundMessage(playerChoice, computerChoice) {
-// //   let roundResult = getRoundResult(playerChoice, computerChoice);
-
-// //   if (roundResult === "tie") {
-// //     prompt("It's a tie!");
-// //   } else if (roundResult === "player") {
-// //     prompt("Player wins!");
-// //   } else {
-// //     prompt("Computer wins!");
-// //   }
-// // }
-
-// const VALID_CHOICES = {
-//   rock: {
-//     validInput: ["rock", "r"],
-//     winConditions: ["scissors", "lizard"],
-//   },
-//   paper: {
-//     validInput: ["paper", "p"],
-//     winConditions: ["rock", "spock"],
-//   },
-//   scissors: {
-//     validInput: ["scissors", "sc"],
-//     winConditions: ["paper", "lizard"],
-//   },
-//   lizard: {
-//     validInput: ["lizard", "l"],
-//     winConditions: ["spock", "paper"],
-//   },
-//   spock: {
-//     validInput: ["spock", "sp"],
-//     winConditions: ["scissors", "rock"],
-//   },
-// };
-
-// const GAME_STATS = {
-//   roundsPlayed: 0,
-//   player: {
-//     wins: 0,
-//     losses: 0,
-//   },
-//   computer: {
-//     wins: 0,
-//     losses: 0,
-//   },
-// };
-
-// const WINNING_SCORE = 3;
-
-// function prompt(message) {
-//   console.log(`=> ${message}`);
-// }
-
-// function replayGame() {
-//   let playerChoice = getPlayerReplayChoice();
-//   updateGameState(playerChoice);
-// }
-
-// function getPlayerReplayChoice() {
-//   prompt("Do you want to play again (y/n)?");
-//   let answer = readline.question().trim().toLowerCase();
-
-//   while (answer[0] !== "n" && answer[0] !== "y") {
-//     prompt("Please enter 'y' or 'n'");
-//     answer = readline.question().toLowerCase();
-//   }
-
-//   return answer[0];
-// }
-
-// function updateGameState(playerReplayChoice) {
-//   if (playerReplayChoice === "n") {
-//     prompt("Thanks for playing, goodbye!");
-//     runGame = false;
-//   } else {
-//     runGame = true;
-//   }
-// }
-
-// function isPlayerWinner(playerChoice, computerChoice) {
-//   const playerChoiceInput = getPlayerChoiceInput(playerChoice);
-//   const winCondition = playerChoiceInput.winConditions;
-
-//   return winCondition.includes(computerChoice);
-// }
-
-// function getPlayerChoiceInput(playerChoice) {
-//   return Object.values(VALID_CHOICES).find((choice) =>
-//     choice.validInput.includes(playerChoice)
-//   );
-// }
-
-// function getRoundResult(playerChoice, computerChoice) {
-//   const FIRST_VALUE_IN_VALID_INPUT = 0;
-
-//   const playerWins = isPlayerWinner(playerChoice, computerChoice);
-//   const playerChoiceInput = getPlayerChoiceInput(playerChoice);
-//   const playerInput = playerChoiceInput.validInput[FIRST_VALUE_IN_VALID_INPUT];
-
-//   if (playerInput === computerChoice) return "tie";
-
-//   return playerWins ? "player" : "computer";
-// }
-
-// function displayRoundWinner(playerChoice, computerChoice) {
-//   prompt(`You chose: ${playerChoice}, computer chose: ${computerChoice}`);
-//   const roundResult = getRoundResult(playerChoice, computerChoice);
-
-//   if (roundResult === "tie") prompt("It's a tie!");
-//   if (roundResult === "player") prompt("Player wins this round!");
-//   if (roundResult === "computer") prompt("Computer wins this round!");
-//   // displayRoundMessage(playerChoice, computerChoice);
-// }
-
-// function incrementScore(winner) {
-//   GAME_STATS[winner].wins += 1;
-//   GAME_STATS[roundsPlayed] += 1;
-// }
-
-// function displayGameWinner() {
-//   let winnerResult = checkIfGameOver();
-//   prompt(winnerResult);
-// }
-
-// function getPlayerChoice() {
-//   let choices = Object.keys(VALID_CHOICES).join(", ");
-
-//   prompt(`Choose one (ex: "rock" or "r"): ${choices}`);
-//   let playerChoice = readline.question();
-
-//   validatePlayerChoice(playerChoice);
-// }
-
-// function validatePlayerChoice(playerChoice) {
-//   const normalizedChoice = playerChoice.toLowerCase();
-
-//   return Object.values(VALID_CHOICES).some((choice) =>
-//     choice.validInput.includes(normalizedChoice)
-//   );
-// }
-
-// function checkIfGameOver() {
-//   incrementScore(winner);
-
-//   if (GAME_STATS.player.wins === WINNING_SCORE) {
-//     return "Player wins!";
-//   } else if (GAME_STATS.computer.wins === WINNING_SCORE) {
-//     return "Computer wins!";
-//   }
-// }
-
-// function generateComputerChoice() {
-//   let validChoicesLength = Object.keys(VALID_CHOICES).length;
-//   let randomIndex = Math.floor(Math.random() * validChoicesLength);
-//   let validChoices = Object.keys(VALID_CHOICES);
-
-//   return validChoices[randomIndex];
-// }
-
-// function playGame() {
-//   prompt(
-//     "Welcome to rock, paper, scissors, spock and lizard!\n First to 3 wins takes home the bacon!"
-//   );
-
-//   let playerChoice = getPlayerChoice();
-//   let computerChoice = generateComputerChoice();
-//   let winner = displayRoundWinner(playerChoice, computerChoice);
-
-//   checkIfGameOver();
-//   displayGameWinner(winner);
-//   console.log(score);
-// }
-
-// while (runGame) {
-//   playGame();
-// }
-
-// /*
-// Keypoints:
-// 1. Functions should only do ONE thing. This keeps them easy to debug and modular!
-
-// */
-
-// /*
-// Edge cases:
-// 1. Handling playerChoice if not in VALID_CHOICES.validInput.
-// 2. Comparing playerChoice vs computerChoice when playerChoice is in abbreviated form.
-
-// */
-
 const readline = require("readline-sync");
 
 let runGame = true;
@@ -283,55 +35,74 @@ const GAME_STATS = {
   },
 };
 
+const MESSAGES = {
+  WELCOME:
+    "Welcome to rock, paper, scissors, spock and lizard!\n First to 3 wins takes home the bacon!\n",
+  INVALID_CHOICE: "That is an invalid choice, try again.",
+  PLAY_AGAIN:
+    "Do you want to play again? Type 'y' to continue or anything else to end the game.",
+  GOODBYE: "Thanks for playing, goodbye!",
+  TIE: "It's a tie!",
+  PLAYER_WINS: "Player wins this round!",
+  COMPUTER_WINS: "Computer wins this round!",
+  GAME_RULES:
+    "GAME RULES:\n Rock: Beats Scissors and Lizard, but loses to Paper and Spock\n Paper: Beats Rock and Spock, but loses to Scissors and Lizard.\n Scissors: Beats Paper and Lizard, but loses to Rock and Spock.\n Lizard: Beats Spock and Paper, but loses to Rock and Scissors.\n Spock: Beats Scissors and Rock, but loses to Paper and Lizard.\n",
+};
+
 const WINNING_SCORE = 3;
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function getPlayerChoice(isValidChoice) {
-  let choices = Object.keys(VALID_CHOICES).join(", ");
+function getInput(message) {
+  prompt(message);
+
+  return readline.question().trim().toLowerCase();
+}
+
+function getPlayerChoice() {
+  const choices = Object.keys(VALID_CHOICES).join(", ");
 
   prompt(`Choose one (ex: "rock" or "r"): ${choices}`);
-  let playerChoice = readline.question();
+  let playerChoice = readline.question().trim();
+
+  while (!isPlayerChoiceValid(playerChoice)) {
+    playerChoice = getInput(MESSAGES.INVALID_CHOICE);
+  }
 
   return playerChoice;
 }
 
-// ! this function not being used:
-function validatePlayerChoice(playerChoice) {
+function isPlayerChoiceValid(playerChoice) {
   const normalizedChoice = playerChoice.toLowerCase();
 
+  // Returns true/false:
   return Object.values(VALID_CHOICES).some((choice) =>
     choice.validInput.includes(normalizedChoice)
   );
 }
 
 function generateComputerChoice() {
-  let validChoicesLength = Object.keys(VALID_CHOICES).length;
-  let randomIndex = Math.floor(Math.random() * validChoicesLength);
-  let validChoices = Object.keys(VALID_CHOICES);
+  const validChoicesLength = Object.keys(VALID_CHOICES).length;
+  const randomIndex = Math.floor(Math.random() * validChoicesLength);
+  const validChoices = Object.keys(VALID_CHOICES);
 
   return validChoices[randomIndex];
 }
 
 function getRoundResult(playerChoice, computerChoice) {
-  const FIRST_VALUE_IN_VALID_INPUT = 0;
+  const playerInput = getPlayerChoiceInput(playerChoice);
+  const playerChoiceName = playerInput.validInput[0];
 
-  const playerWins = isPlayerWinner(playerChoice, computerChoice);
-  const playerChoiceInput = getPlayerChoiceInput(playerChoice);
-  const playerInput = playerChoiceInput.validInput[FIRST_VALUE_IN_VALID_INPUT];
+  if (playerChoiceName === computerChoice) return "tie";
 
-  if (playerInput === computerChoice) return "tie";
-
-  return playerWins ? "player" : "computer";
+  return isPlayerWinner(playerInput, computerChoice) ? "player" : "computer";
 }
 
-function isPlayerWinner(playerChoice, computerChoice) {
-  const playerChoiceInput = getPlayerChoiceInput(playerChoice);
-  const winCondition = playerChoiceInput.winConditions;
-
-  return winCondition.includes(computerChoice);
+function isPlayerWinner(playerInput, computerChoice) {
+  const winConditions = playerInput.winConditions;
+  return winConditions.includes(computerChoice);
 }
 
 function getPlayerChoiceInput(playerChoice) {
@@ -341,11 +112,12 @@ function getPlayerChoiceInput(playerChoice) {
 }
 
 function displayRoundWinner(playerChoice, computerChoice, roundResult) {
-  prompt(`You chose: ${playerChoice}, computer chose: ${computerChoice}`);
+  const fullPlayerChoice = getPlayerChoiceInput(playerChoice).validInput[0];
+  prompt(`You chose: ${fullPlayerChoice}, computer chose: ${computerChoice}`);
 
-  if (roundResult === "tie") prompt("It's a tie!");
-  if (roundResult === "player") prompt("Player wins this round!");
-  if (roundResult === "computer") prompt("Computer wins this round!");
+  if (roundResult === "tie") prompt(MESSAGES.TIE);
+  if (roundResult === "player") prompt(MESSAGES.PLAYER_WINS);
+  if (roundResult === "computer") prompt(MESSAGES.COMPUTER_WINS);
 }
 
 function incrementScore(winner) {
@@ -354,15 +126,13 @@ function incrementScore(winner) {
   } else {
     GAME_STATS[winner].wins += 1;
   }
-
-  GAME_STATS.roundsPlayed += 1;
 }
 
 function isGameOver() {
   if (GAME_STATS.player.wins === WINNING_SCORE) {
-    return "Player wins the game!";
+    return `*********** ${MESSAGES.PLAYER_WINS} ***********`;
   } else if (GAME_STATS.computer.wins === WINNING_SCORE) {
-    return "Computer wins the game!";
+    return `*********** ${MESSAGES.COMPUTER_WINS} ***********`;
   }
 }
 
@@ -375,14 +145,12 @@ function displayGameScore() {
 }
 
 function askForGameReplay() {
-  prompt("Do you want to play again?");
-  let answer = readline.question().trim().toLowerCase();
+  const answer = getInput(MESSAGES.PLAY_AGAIN);
 
   if (answer[0] !== "y") {
     runGame = false;
+    prompt(MESSAGES.GOODBYE);
   }
-  console.clear();
-  prompt("Thanks for playing, goodbye!");
 }
 
 function resetGameStats() {
@@ -391,19 +159,26 @@ function resetGameStats() {
   GAME_STATS.computer.wins = 0;
 }
 
-function playGame() {
-  prompt(
-    "Welcome to rock, paper, scissors, spock and lizard!\n First to 3 wins takes home the bacon!"
+function incrementRound() {
+  GAME_STATS.roundsPlayed += 1;
+
+  console.log(
+    `################ ROUND #${GAME_STATS.roundsPlayed} ################`
   );
+}
 
+function playGame() {
+  if (GAME_STATS.roundsPlayed === 0) {
+    prompt(MESSAGES.WELCOME);
+    prompt(MESSAGES.GAME_RULES);
+  }
+
+  incrementRound();
   const playerChoice = getPlayerChoice();
-  const isPlayerChoiceValid = validatePlayerChoice(playerChoice);
-
   const computerChoice = generateComputerChoice();
-
   const roundResult = getRoundResult(playerChoice, computerChoice);
   displayRoundWinner(playerChoice, computerChoice, roundResult);
-  incrementScore(winner);
+  incrementScore(roundResult);
   displayGameScore();
 
   const gameWinner = isGameOver();
@@ -411,6 +186,7 @@ function playGame() {
     displayGameWinner(gameWinner);
     askForGameReplay();
     resetGameStats();
+    console.clear();
   }
 }
 
